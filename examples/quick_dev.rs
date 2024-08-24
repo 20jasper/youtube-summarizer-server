a6f55b74928cf092e8646a20ef21e12ff9874ef4
Jacob Asper <jacobasper191@gmail.com>
2024-08-24 16:54:30 -0400

    Get transcripts by url

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -5,10 +5,13 @@ use serde_json::json;
 async fn main() -> Result<()> {
 	let hc = httpc_test::new_client("http://localhost:8080")?;
 
-	hc.do_post("/transcript", json!({"url": "youtube.com"}))
-		.await?
-		.print()
-		.await?;
+	hc.do_post(
+		"/transcript",
+		json!({"url": "https://www.youtube.com/watch?v=r2s0e64iSJI"}),
+	)
+	.await?
+	.print()
+	.await?;
 
 	Ok(())
 }
--- src/web/routes/transcript.rs
+++ src/web/routes/transcript.rs
@@ -1,21 +1,23 @@
-use axum::{
-	extract::{Path, Query},
-	response::Html,
-	routing::{get, post},
-	Json, Router,
-};
+use axum::{routing::post, Json, Router};
 use axum_macros::debug_handler;
 use serde::Deserialize;
 use serde_json::{json, Value};
 
+use crate::web::services::transcript::{self, clean_vtt};
+
 #[derive(Deserialize)]
 struct TranscriptParams {
 	url: String,
+	raw: Option<bool>,
 }
 
 #[debug_handler]
-async fn transcript(Json(TranscriptParams { url }): Json<TranscriptParams>) -> Json<Value> {
-	Json(json!({ "url": url }))
+async fn transcript(Json(TranscriptParams { url, raw }): Json<TranscriptParams>) -> Json<Value> {
+	let mut transcript = transcript::get_by_url(&url).unwrap();
+	if !raw.unwrap_or_default() {
+		transcript = clean_vtt(&transcript);
+	}
+	Json(json!({ "url": url, "transcript": transcript }))
 }
 
 pub fn routes() -> Router {
