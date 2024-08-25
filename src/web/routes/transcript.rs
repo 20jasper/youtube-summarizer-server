a63e6a9b90f7636574dce4bf69e24983d750f899
Jacob Asper <jacobasper191@gmail.com>
2024-08-24 22:01:01 -0400

    add nice log and small refactor

--- src/web/routes/transcript.rs
+++ src/web/routes/transcript.rs
@@ -8,13 +8,16 @@ use crate::web::services::transcript::{self, clean_vtt};
 #[derive(Deserialize)]
 struct TranscriptParams {
 	url: String,
-	raw: Option<bool>,
+	#[serde(default)]
+	raw: bool,
 }
 
 #[debug_handler]
 async fn transcript(Json(TranscriptParams { url, raw }): Json<TranscriptParams>) -> Json<Value> {
+	println!("post transcript: {url:?}, raw {raw:?}");
+
 	let mut transcript = transcript::get_by_url(&url).unwrap();
-	if !raw.unwrap_or_default() {
+	if !raw {
 		transcript = clean_vtt(&transcript);
 	}
 	Json(json!({ "url": url, "transcript": transcript }))
