88fcd9eef853471b39b0e49c3fc2067c1fa3b9c3
Jacob Asper <jacobasper191@gmail.com>
2025-07-31 01:03:29 -0400

    add derive_more

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use crate::error::Error;
 use crate::error::Result;
 use dotenvy::dotenv;
 use std::env;
@@ -12,9 +13,14 @@ impl Completion {
 	pub fn build() -> Result<Self> {
 		dotenv()?;
 
-		let api_key = env::var("OPEN_AI_API_KEY").map_err(|_| "OPEN_AI_API_KEY is not set")?;
-		let model = env::var("OPEN_AI_MODEL").map_err(|_| "OPEN_AI_MODEL is not set")?;
-		let base_url = env::var("OPEN_AI_BASE_URL").map_err(|_| "OPEN_AI_BASE_URL is not set")?;
+		const OPEN_AI_API_KEY: &str = "OPEN_AI_API_KEY";
+		const OPEN_AI_MODEL: &str = "OPEN_AI_MODEL";
+		const OPEN_AI_BASE_URL: &str = "OPEN_AI_BASE_URL";
+
+		let api_key = env::var(OPEN_AI_API_KEY).map_err(|_| Error::EnvMissing(OPEN_AI_API_KEY))?;
+		let model = env::var(OPEN_AI_MODEL).map_err(|_| Error::EnvMissing(OPEN_AI_MODEL))?;
+		let base_url =
+			env::var(OPEN_AI_BASE_URL).map_err(|_| Error::EnvMissing(OPEN_AI_BASE_URL))?;
 
 		Ok(Self {
 			api_key,
@@ -24,6 +30,8 @@ impl Completion {
 	}
 }
 
+const YOUTUBE_PROXY: &str = "YOUTUBE_PROXY";
+const YOUTUBE_RETRIES: &str = "YOUTUBE_RETRIES";
 pub struct Youtube {
 	pub retries: u8,
 	pub proxy: String,
@@ -33,11 +41,11 @@ impl Youtube {
 	pub fn build() -> Result<Self> {
 		dotenv()?;
 
-		let retries = env::var("YOUTUBE_RETRIES")
+		let retries = env::var(YOUTUBE_RETRIES)
 			.ok()
 			.and_then(|s| s.parse::<u8>().ok())
 			.unwrap_or(3);
-		let proxy = env::var("YOUTUBE_PROXY").map_err(|_| "YOUTUBE_PROXY is not set")?;
+		let proxy = env::var(YOUTUBE_PROXY).map_err(|_| Error::EnvMissing(YOUTUBE_PROXY))?;
 
 		Ok(Self { retries, proxy })
 	}
--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,17 @@
-// TODO make custom error for parsing env vars
-pub type Error = Box<dyn std::error::Error>;
+use derive_more::From;
+use displaydoc::Display;
+use thiserror::Error;
+
+#[derive(Display, Debug, Error, From)]
+pub enum Error {
+	/// Environment variable `{0}` is not set
+	EnvMissing(String),
+	/// Error parsing environment variables: {0}
+	EnvParse(#[from] dotenvy::Error),
+
+	/// Error: {0:?}
+	#[error(transparent)]
+	Other(#[from] Box<dyn std::error::Error>),
+}
 
 pub type Result<T> = core::result::Result<T, Error>;
