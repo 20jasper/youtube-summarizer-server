b15d6a41ada3f585143b4fcc616b5604c9e47626
Jacob Asper <jacobasper191@gmail.com>
2025-07-31 02:38:08 -0400

    refactor routes to use custom status code mappings

--- src/web/routes/transcript.rs
+++ src/web/routes/transcript.rs
@@ -4,6 +4,8 @@ use axum_macros::debug_handler;
 use serde::Deserialize;
 use serde_json::{json, Value};
 
+use crate::error::Result;
+
 #[derive(Deserialize)]
 struct TranscriptParams {
 	url: String,
@@ -13,30 +15,18 @@ struct TranscriptParams {
 
 async fn transcript(
 	Query(TranscriptParams { url, raw }): Query<TranscriptParams>,
-) -> (StatusCode, Json<Value>) {
+) -> Result<(StatusCode, Json<Value>)> {
 	println!("GET transcript {url:?}, raw {raw:?}");
 
-	match transcript::get_transcript_by_url(&url, raw).await {
-		Ok(transcript) => {
-			println!("got transcript");
-			(
-				StatusCode::OK,
-				Json(json!(
-						{
-							"url": url,
-							"transcript": transcript
-						}
-				)),
-			)
-		}
-		Err(e) => {
-			println!("failed to get transcript {e:?}");
-			(
-				StatusCode::INTERNAL_SERVER_ERROR,
-				Json(json!({"message": "internal server error"})),
-			)
-		}
-	}
+	Ok((
+		StatusCode::OK,
+		Json(json!(
+				{
+					"url": url,
+					"transcript":transcript::get_transcript_by_url(&url, raw).await?
+				}
+		)),
+	))
 }
 
 #[derive(Deserialize)]
@@ -46,25 +36,18 @@ struct SummaryParams {
 #[debug_handler]
 async fn summarize(
 	Query(SummaryParams { url }): Query<SummaryParams>,
-) -> (StatusCode, Json<Value>) {
-	match transcript::summarize_by_url(&url).await {
-		Ok(transcript) => (
-			StatusCode::OK,
-			Json(json!(
-					{
-						"url": url,
-						"transcript": transcript
-					}
-			)),
-		),
-		Err(e) => {
-			println!("failed to get transcript {e:?}");
-			(
-				StatusCode::INTERNAL_SERVER_ERROR,
-				Json(json!({"message": "internal server error"})),
-			)
-		}
-	}
+) -> Result<(StatusCode, Json<Value>)> {
+	println!("GET summary {url:?}");
+
+	Ok((
+		StatusCode::OK,
+		Json(json!(
+				{
+					"url": url,
+					"summary":transcript::summarize_by_url(&url).await?
+				}
+		)),
+	))
 }
 
 pub fn routes() -> Router {
