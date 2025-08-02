1f1e11b73125108f7d1a5ca680b02f0b59178f03
Jacob Asper <jacobasper191@gmail.com>
2025-08-02 16:34:10 -0400

    Move caching from transcript route to service and remove CLI

--- src/web/routes/transcript.rs
+++ src/web/routes/transcript.rs
@@ -16,34 +16,11 @@ async fn transcript(
 	Query(TranscriptParams { url }): Query<TranscriptParams>,
 	State(pool): State<PgPool>,
 ) -> Result<(StatusCode, Json<Value>)> {
-	let url = YTUrl::parse_from_str(&url)?;
-
-	let transcript = if let Ok(row) =
-		sqlx::query!("SELECT subtitles FROM videos WHERE video_id = $1", url.id())
-			.fetch_one(&pool)
-			.await
-	{
-		tracing::debug!("found transcript in database");
-		row.subtitles
-	} else {
-		let transcript = transcript::get_transcript_by_url(&url).await?;
-
-		sqlx::query!(
-			"INSERT INTO videos (video_id, subtitles) VALUES ($1, $2)",
-			url.id(),
-			transcript
-		)
-		.execute(&pool)
-		.await?;
-
-		transcript
-	};
-
 	Ok((
 		StatusCode::OK,
 		Json(json!(
 				{
-					"transcript": transcript
+					"transcript": transcript::get_transcript_by_url(&YTUrl::parse_from_str(&url)?, &pool).await?
 				}
 		)),
 	))
@@ -75,7 +52,7 @@ async fn summarize(
 		row.summary
 			.expect("Summary should not be null")
 	} else {
-		let summary = transcript::summarize_by_url(&url).await?;
+		let summary = transcript::summarize_by_url(&url, &pool).await?;
 
 		sqlx::query!(
 			"UPDATE videos SET summary = $1 WHERE video_id = $2",
