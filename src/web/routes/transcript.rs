8f2fa8938aa7de8cb07a6bf22b7b947e3a2899fb
Jacob Asper <jacobasper191@gmail.com>
2025-08-06 23:19:55 -0400

    Add ratings endpoint and form

--- src/web/routes/transcript.rs
+++ src/web/routes/transcript.rs
@@ -1,10 +1,12 @@
-use crate::error::Result;
+use crate::error::{Error, Result};
 use crate::web::clients::DeepInfraClient;
 use crate::web::services::transcript;
 use crate::web::services::youtube::YtDlpService;
 use crate::web::utils::YTUrl;
 use axum::extract::State;
+use axum::routing::post;
 use axum::{Json, Router, extract::Query, http::StatusCode, routing::get};
+use axum_macros::debug_handler;
 use serde::Deserialize;
 use serde_json::{Value, json};
 use sqlx::PgPool;
@@ -62,8 +64,61 @@ async fn summarize(
 	))
 }
 
+#[derive(Deserialize, Debug, PartialEq, Eq, sqlx::Type)]
+#[serde(rename_all = "camelCase")]
+#[sqlx(type_name = "rating")]
+#[sqlx(rename_all = "lowercase")]
+enum Rating {
+	Like,
+	Dislike,
+}
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Rate {
+	video_id: String,
+	rating: Rating,
+	message: Option<String>,
+}
+#[instrument(skip(pool))]
+#[debug_handler]
+async fn rate(
+	State(pool): State<PgPool>,
+	Json(Rate {
+		video_id,
+		rating,
+		message,
+	}): Json<Rate>,
+) -> Result<(StatusCode, ())> {
+	#[allow(
+		clippy::as_conversions,
+		reason = "casting rating as rating to make sqlx happy"
+	)]
+	let res = sqlx::query!(
+		"
+			INSERT INTO ratings (video_id, rating, message)
+			SELECT $1::VARCHAR, $2, $3
+			WHERE EXISTS (
+				SELECT *
+				FROM videos
+				WHERE video_id = $1 AND summary IS NOT NULL
+			)
+		",
+		video_id,
+		rating as Rating,
+		message,
+	)
+	.execute(&pool)
+	.await?;
+
+	if res.rows_affected() == 0 {
+		return Err(Error::NotFound);
+	}
+	Ok((StatusCode::OK, ()))
+}
+
 pub fn routes() -> Router<PgPool> {
 	Router::new()
 		.route("/summary", get(summarize))
+		.route("/summary/rating", post(rate))
 		.route("/transcript", get(transcript))
 }
