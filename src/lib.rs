3c8d53e3e36fec7f9a49eba616e6d3b754dd18e9
Jacob Asper <jacobasper191@gmail.com>
2025-09-01 16:24:52 -0400

    simplify run return types

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
 use crate::web::{routes::routes, services::env::ApplicationSettings};
 use sqlx::PgPool;
+use std::io;
 use tokio::net::TcpListener;
 
 pub mod error;
 pub mod prompts;
 pub mod web;
 
-pub fn run(
+pub async fn run(
 	listener: TcpListener,
 	pool: PgPool,
 	application: &ApplicationSettings,
-) -> axum::serve::Serve<TcpListener, axum::routing::IntoMakeService<axum::Router>, axum::Router> {
+) -> io::Result<()> {
 	let routes = routes(pool, application);
-
-	axum::serve(listener, routes.into_make_service())
+	axum::serve(listener, routes.into_make_service()).await
 }
