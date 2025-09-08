e1b50bd4a17cd94ce1976bff41af1440c36b4bcd
Jacob Asper <jacobasper191@gmail.com>
2025-09-07 23:59:48 -0400

    Add trace id per request

--- src/web/routes.rs
+++ src/web/routes.rs
@@ -1,8 +1,9 @@
 use crate::web::services::{env::ApplicationSettings, youtube::YtService};
-use axum::{Router, routing::get};
+use axum::{Router, body::Body, http::Request, routing::get};
 use sqlx::PgPool;
 use std::sync::Arc;
 use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing::Level;
 
 pub mod summary;
 
@@ -18,6 +19,18 @@ pub fn routes(app_state: AppState, app: &ApplicationSettings) -> Router {
 		.nest("/summary", summary::routes())
 		// layers run from bottom to top
 		.fallback_service(ServeDir::new(app.public_dir.clone()))
-		.layer(TraceLayer::new_for_http())
+		.layer(
+			TraceLayer::new_for_http().make_span_with(|req: &Request<Body>| {
+				let request_id = uuid::Uuid::new_v4();
+				tracing::span!(
+					Level::DEBUG,
+					"request",
+					method = tracing::field::display(req.method()),
+					uri = tracing::field::display(req.uri()),
+					version = tracing::field::debug(req.version()),
+					request_id = tracing::field::display(request_id)
+				)
+			}),
+		)
 		.with_state(app_state)
 }
