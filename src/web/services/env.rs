8ac4e7e92af5569573e32fc25e5bb0fa842033a8
Jacob Asper <jacobasper191@gmail.com>
2026-01-31 21:32:36 -0500

    reduce tracing verbosity

--- src/web/services/env.rs
+++ src/web/services/env.rs
@@ -129,7 +129,7 @@ pub struct AxiomSettings {
 }
 
 fn rust_log() -> String {
-	"youtube_summarizer_server=trace,tower_http=debug,reqwest=debug".into()
+	"youtube_summarizer_server=info,tower_http=warn,reqwest=warn,h2=warn".into()
 }
 #[derive(serde::Deserialize, Clone, Debug)]
 pub struct RustSettings {
